31414484899bb1400863e23c9f91581d56eb0910
Everett C. Haimes III <everett@powernode.net>
2011-12-19 14:01:59 -0900

    New poller integration, quote cleanup.

--- powernode_manager.rb
+++ powernode_manager.rb
@@ -1,20 +1,20 @@
-require 'aws'
-require 'benchmark'
-require 'rubygems'
-require 'bundler'
-require 'restclient'
-require 'beetle'
-require 'json'
-require 'net/ssh'
-require 'net/sftp'
-require 'tmpdir'
+require "aws"
+require "benchmark"
+require "rubygems"
+require "bundler"
+require "restclient"
+require "beetle"
+require "json"
+require "net/ssh"
+require "net/sftp"
+require "tmpdir"
 
-APP_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), "config.yml"))
+APP_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), "manager_config.yml"))
 
-#RestClient.log = 'stdout'
+#RestClient.log = "stdout"
 
 Beetle.config do |config|
-  config.logger.level = Logger::DEBUG
+  #config.logger.level = Logger::DEBUG
   config.servers = APP_CONFIG['beetle_servers']
   config.redis_server = APP_CONFIG['redis_server']
   config.redis_servers = APP_CONFIG['redis_servers']
@@ -26,30 +26,29 @@ $beetle.register_message(:powernode)
 
 $beetle.register_handler(:powernode, :exceptions => 1, :delay => 0) do |message|
   params = JSON.parse(message.data)
-  operation = params["operation"]
+  operation = params['operation']
+  @node_platform = params['node_platform']
+  @node_platform_resource = RestClient::Resource.new(@node_platform['url'] + "/manage/platform",
+                                                     @node_platform['identifier'],
+                                                     @node_platform['passphrase'])
+  @node_response = JSON.parse(@node_platform_resource["nodes/#{params['node']['identifier']}"].get(:accept => :json))
 
   fork { self.send(operation, params) } if self.respond_to?(operation)
 end
 
-$node_platform_resource = RestClient::Resource.new(APP_CONFIG['platform_url'],
-                                                   APP_CONFIG['platform_identifier'],
-                                                   APP_CONFIG['platform_passphrase'])
-
 def self.node_trigger_update(params)
-  node_response = JSON.parse($node_platform_resource["nodes/#{params['node']['identifier']}"].get(:accept => :json))
-
-  node = node_response['node']
-  node_instances = node_response['node_instances']
-  node_module_categories = node_response['node_module_categories']
-  node_template = node_response['node_template']
+  node = @node_response['node']
+  node_instances = @node_response['node_instances']
+  node_module_categories = @node_response['node_module_categories']
+  node_template = @node_response['node_template']
 
   if node['enabled'] == true
-    $node_platform_resource["nodes/#{node['identifier']}"].post(:trigger_update => true)
+    @node_platform_resource["nodes/#{node['identifier']}"].post(:last_update => Time.now, :trigger_update => true)
     node_instances.each do |node_instance|
-      if node_instance['last_update'].nil? || Time.parse(node_instance['last_update']) < APP_CONFIG['poll_interval'].seconds.ago
+      if node_instance['last_update'].nil? || Time.parse(node_instance['last_update']) < APP_CONFIG['update_timeout'].seconds.ago
         puts "Triggering update on instance: #{node_instance['aws_instance']}..."
         node_instance['last_update'] = Time.now
-        $node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance)
+        @node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance)
         #begin
           session = Net::SSH.start(node_instance['ip_private'],
                                    node_template['admin_user'],
@@ -71,23 +70,21 @@ def self.node_trigger_update(params)
 end
 
 def self.node_update_status(params)
-  node_response = JSON.parse($node_platform_resource["nodes/#{params['node']['identifier']}"].get(:accept => :json))
-
-  node = node_response['node']
-  node_instances = node_response['node_instances']
-  node_platform = node_response['node_platform']
-  node_template = node_response['node_template']
+  node = @node_response['node']
+  node_instances = @node_response['node_instances']
+  node_template = @node_response['node_template']
   instance_variance = node['instances'] - node_instances.count
 
-  @ec2 = Aws::Ec2.new(node_platform['aws_access_key'],
-                      node_platform['aws_secret_key'],
-                      {:endpoint_url => node_platform['aws_url']})
-
+  puts "Updating node: #{node['identifier']}..."
+  @node_platform_resource["nodes/#{node['identifier']}"].post(:last_update => Time.now)
+  @ec2 = Aws::Ec2.new(@node_platform['aws_access_key'],
+                      @node_platform['aws_secret_key'],
+                      {:endpoint_url => @node_platform['aws_url']})
   if node['enabled'] == true
     check_instances(node, node_instances)
     if instance_variance > 0
       puts "Launching #{instance_variance} instances..."
-      launch_instances(node, node_template, node_platform, instance_variance)
+      launch_instances(node, node_template, instance_variance)
     elsif instance_variance < 0
       instance_variance *= -1
       puts "Destroying #{instance_variance} instances..."
@@ -105,22 +102,16 @@ def self.node_update_status(params)
 end
 
 def self.node_module_commit(params)
-  node = params['node']
+  node = @node_response['node']
+  node_template = @node_response['node_template']
+  node_instance = params['node_instance']
   node_module = params['node_module']
-  node_template = params['node_template']
-  puts node_instance = params['node_instance']
-  #node_module_category = params['node_module_category']
-  #node_module_identifier = params['node_module_identifier']
-  #node_module_spec = params['node_module_spec']
-  #node_platform = params['node_platform']
 
   #if node_instance['state'] == "active"
   puts "Committing module #{node_module['name']} from node #{node['identifier']}..."
 
   tmp_dir = Dir.mktmpdir
   FileUtils.chmod(0755, tmp_dir)
-
-  puts "Spec: #{node_module['spec']}"
 
   Net::SFTP.start(node_instance['ip_private'], node_template['admin_user'], :key_data => node['key'], :paranoid => false) do |session|
     node_module['spec'].each_line do |file|
@@ -131,22 +122,22 @@ def self.node_module_commit(params)
         case session.lstat!(file).type
         when 1
           # File discovered, create dir and download.
-          FileUtils.mkdir_p(target_path[0..target_path.rindex('/')])
+          FileUtils.mkdir_p(target_path[0..target_path.rindex("/")])
           session.download!(file, target_path)
         when 2
           # Directory discovered, create dir.
           FileUtils.mkdir_p(target_path)
         when 3
           # Symlink discovered, create dir and symlink.
-          FileUtils.mkdir_p(target_path[0..target_path.rindex('/')])
+          FileUtils.mkdir_p(target_path[0..target_path.rindex("/")])
           FileUtils.ln_s(session.realpath!(file).name, target_path)
         end
       #rescue
       #end
 
       #path = file
-      #while path.rindex('/') > 0 do
-      #  path = path[0..path.rindex('/')-1]
+      #while path.rindex("/") > 0 do
+      #  path = path[0..path.rindex("/")-1]
       #  puts "Path: #{path}"
       #  puts "Attributes: #{session.lstat!(file).attributes}"
       #  puts FileUtils.chown(session.lstat!(path).attributes[:uid], session.lstat!(path).attributes[:gid], tmp_dir + path)
@@ -163,7 +154,7 @@ def self.node_module_commit(params)
   tmp_module = Tempfile.new("module-#{node['id']}")
   tmp_module.close
   system("mksquashfs #{tmp_dir} #{tmp_module.path} -noappend")
-  $node_platform_resource["nodes/#{node['identifier']}/modules/#{node_module['identifier']}.json"].post(
+  @node_platform_resource["nodes/#{node['identifier']}/modules/#{node_module['identifier']}.json"].post(
     :accept => :json,
     :data => File.open(tmp_module),
     :multipart => true,
@@ -184,8 +175,8 @@ def check_instances(node, node_instances)
 
   node_instances.each do |node_instance|
     if aws_instance = aws_instances.find {|i| i[:aws_instance_id] == node_instance['aws_instance']}
-      node_instance.delete('id')
-      node_instance.delete('node_id')
+      node_instance.delete("id")
+      node_instance.delete("node_id")
       node_instance['error_count'] = 0
       node_instance['ip_private'] = aws_instance[:private_dns_name]
       node_instance['state'] = aws_instance[:aws_state]
@@ -193,13 +184,13 @@ def check_instances(node, node_instances)
       if node_instance['error_count'] < 3
         node_instance['error_count'] += 1
       else
-        $node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance, :operation => 'destroy')
+        @node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance, :operation => "destroy")
       end
     end
   end
 
   #begin
-    $node_platform_resource["nodes/#{node['identifier']}/instances.json"].post(:node_instances => node_instances.to_json)
+    @node_platform_resource["nodes/#{node['identifier']}/instances.json"].post(:node_instances => node_instances.to_json)
   #rescue
   #end
 
@@ -215,13 +206,13 @@ def destroy_instances(node, node_instances, count = 1)
       #rescue
       #end
       #begin
-        $node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance, :operation => 'destroy')
+        @node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance, :operation => "destroy")
       #rescue
       #end
   end
 end
 
-def launch_instances(node, node_template, node_platform, count = 1)
+def launch_instances(node, node_template, count = 1)
   puts "Loading key for Node ID #{node['id']}..."
   keypair_name = "node-#{node['id']}"
   keys = @ec2.describe_key_pairs([keypair_name])
@@ -230,13 +221,13 @@ def launch_instances(node, node_template, node_platform, count = 1)
     key = keys[0]
   else
     @ec2.delete_key_pair(keypair_name)
-    key = ec2.create_key_pair(keypair_name)
-    $node_platform_resource['.json'].post(key)
+    key = @ec2.create_key_pair(keypair_name)
+    @node_platform_resource.post(key, :accept => :json)
   end
 
   user_data = <<END
 #!/bin/sh
-PARENT=#{node_platform['url']}
+PARENT=#{@node_platform['url']}
 IDENTIFIER=#{node['identifier']}
 PASSPHRASE=#{node['passphrase']}
 END
@@ -262,7 +253,7 @@ END
       node_instance['start_time'] = aws_instance[:aws_launch_time]
 
     #begin
-      $node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance)
+      @node_platform_resource["nodes/#{node['identifier']}/instance.json"].post(:node_instance => node_instance)
     #rescue
     #end
   end
