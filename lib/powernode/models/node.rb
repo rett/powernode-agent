5ea43e020d8e5edd26744061a7b9e1614ecb64f6
Everett C. Haimes III <everett@powernode.net>
2014-12-20 00:35:33 -0900

    Only update local ssh key file if contents differ from actual key data.

--- lib/powernode/models/node.rb
+++ lib/powernode/models/node.rb
@@ -200,11 +200,13 @@ class Node
   def ssh_key_file
     FileUtils.mkdir_p(Powernode.config(:ssh_key_dir)) unless Dir.exist?(Powernode.config(:ssh_key_dir))
     key_file = File.join(Powernode.config(:ssh_key_dir), "#{id}.pem")
-    File.open(key_file, File::RDWR|File::CREAT, 0600) do |f|
-      f.flock(File::LOCK_EX)
-      f.write(ssh_key_data.to_pem)
-      f.flush
-      f.truncate(f.pos)
+    unless ssh_key_data.to_pem == File.read(key_file)
+      File.open(key_file, File::RDWR|File::CREAT, 0600) do |f|
+        f.flock(File::LOCK_EX)
+        f.write(ssh_key_data.to_pem)
+        f.flush
+        f.truncate(f.pos)
+      end
     end
     key_file
   end
